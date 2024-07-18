aa457771f2f9f73ea34bcb288baec9ccf8266836
pratham0709 <prathameshjadhav792@gmail.com>
2024-07-18 21:11:39 +0530

    New Changes

--- Server/controllers/Auth.js
+++ Server/controllers/Auth.js
@@ -1,8 +1,35 @@
 const User = require("../models/User");
 const OTP = require("../models/OTP");
-
+const otpGenerator = require("otp-generator"); 
 //sendOTP
+exports.sendOTP = async (req,res) => {
+    try{
+        // fetch email from request body
+        const {email} = req.body;
 
+        // check user is already present
+        const checkUserPresent = await User.findOne({email});
+
+        //if user aleready exit, then return a responce
+        if(checkUserPresent){
+            return res.status(401).json({
+                success:false,
+                message:"User is already registered",
+            })
+        }
+
+        //generate OTP
+        var otp = otpGenerator.generate(6, {
+            upperCaseAlphabets:false,
+            lowerCaseAlphabets:false,
+            specialChars:false,
+        });
+        console.log("OTP Generated", otp);
+    }
+    catch(error){
+
+    }
+}
 
 // signUp
 
