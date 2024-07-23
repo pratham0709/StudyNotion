39623967d186cc0834c8299270d285aa37eaad02
pratham0709 <prathameshjadhav792@gmail.com>
2024-07-23 14:08:44 +0530

    create controller for course

--- Server/controllers/Course.js
+++ Server/controllers/Course.js
@@ -1,3 +1,57 @@
 const Tag = require("../models/tags");
 const User = require("../models/User");
 const Course = require("../models/Course")
+
+
+//createCourse handler function
+exports.createCourse = async (req,res) => {
+    try{
+        // fetch data
+        const {courseName, courseDescription, whatYouWillLearn, price, tag} = req.body;
+
+        // get the thumbnail
+        const thumbnail = req.files.thumbnailImage;
+
+        // validation 
+        if(!courseName || !courseDescription || !whatYouWillLearn || !price || !tag){
+            return res.status(400).json({
+                success:false,
+                message:"All fields are required",
+            })
+        }
+
+        // check for Instructor
+        const userId = req.user.id;
+        const instructorDetails = await User.findById(userId);
+        console.log("Instructor Details: " , instructorDetails);
+
+        if(!instructorDetails){
+            return res.status(401).json({
+                success:false,
+                message:"Instructor Details not found",
+            })
+        }
+
+        const tagDetails = await Tag.findById(tag);
+
+        if(!tagDetails){
+            return res.status(401).json({
+                success:false,
+                message:"Tags Details are not found",
+            })
+        }
+
+        // upload image to cloudinary
+        const thumbnailImage =await uploadImageToCloudinary(thumbnail, process.env.FOLDER_NAME); 
+
+        // create an entry in db
+        const newCourse = await Course.create({
+            courseName,
+            courseDescription,
+            instructor: instructorDetails._id,
+        })
+    }
+    catch(error){
+
+    }
+}
