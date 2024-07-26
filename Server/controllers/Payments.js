f001bcc358d955e4eed91967f17e37ffea44606f
pratham0709 <prathameshjadhav792@gmail.com>
2024-07-26 18:51:28 +0530

    Verify Payment Gateway controller

--- Server/controllers/Payments.js
+++ Server/controllers/Payments.js
@@ -85,3 +85,75 @@ exports.capturePayment = async (req,res) => {
             });
         }
     }
+
+
+// verify signature
+exports.verifySignature = async (req,res) => {
+    const webhookSecret = "12345678";
+
+    const signature = req.headers["x-razorpay-signature"];
+
+    const shasum = crypto.createHmac("sha256", webhookSecret);
+    shasum.update(JSON.stringify(req.body));
+    const digest = shasum.digest("hex");
+
+    if(signature === digest){
+        console.log("Payment is Authorised");
+
+        const {courseId, userId} = req.body.payload.payment.entity.notes;
+
+        try{
+            // full fil the action
+
+            //find the course and enrolled the student in it
+            const enrolledCourse = await Course.findOneAndUpdate(
+                                                {_id:courseId},
+                                                {$push:{studentEnrolled:userId}},
+                                                {new:true},
+            )
+
+            if(!enrolledCourse){
+                return res.status(500).json({
+                    success:false,
+                    message:'Course not Found',
+                });
+            }
+
+            console.log(enrolledCourse);
+
+            //find the student and add the course to their list of enrolled course me
+            const enrolledStudent = await User.findOneAndUpdate(
+                                                {_id:userId},
+                                                {$push:{courses:courseId}},
+                                                {new:true}
+            );
+            console.log(enrolledStudent);
+
+            // amil send krna hai confirmation wala
+            const emailResponce = await mailSender(
+                                        enrolledStudent.email,
+                                        "Congratulation from CodeWithPratham",
+                                        "Congratulations, you are onboarded into new CodeHelp Course"
+            );
+            console.log(emailResponce);
+            return res.status(200).json({
+                success:true,
+                message:"Signature Verifed and Course Added"
+            })
+        }
+        catch(error){
+            console.log(error);
+            return res.status(500).json({
+                success:false,
+                message:error.message,
+            })
+        }
+    }
+
+    else{
+        return res.status(400).json({
+            success:false,
+            message:"Could not find the Course",
+        })
+    }
+} 
