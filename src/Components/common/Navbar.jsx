9dfc9cd62cb786d8790dd2ace17b1ef11075c02a
pratham0709 <prathameshjadhav792@gmail.com>
2024-08-07 22:48:31 +0530

    New Changes

--- src/Components/common/Navbar.jsx
+++ src/Components/common/Navbar.jsx
@@ -1,16 +1,47 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import logo from '../../assets/Logo/Logo-Full-Light.png'
 import { Link, matchPath, useLocation } from 'react-router-dom'
 import { NavbarLinks } from '../../data/navbar-links'
 import { useSelector } from 'react-redux'
 import { FaOpencart } from "react-icons/fa";
 import ProfileDropDown from '../Core/Auth/ProfileDropDown'
+import { apiConnector} from '../../services/apiconnector'
+import { categories } from '../../services/apis'
+import { IoMdArrowDropdownCircle } from "react-icons/io";
+
+const subLinks = [
+    {
+        title : "Python",
+        link: "/catalog/python"
+    },
+    {
+        title: "web dev",
+        link: "/catalog/web-development"
+    }
+]
 
 const Navbar = () => {
 
     const {token} = useSelector( (state) => state.auth);
     const {user} = useSelector( (state) => state.profile);
     const {totalItems} = useSelector( (state) => state.cart);
+
+    // const [subLinks, setSubLinks] = useState([]);
+   
+    // const fetchSublinks =  async() => {
+    //     try{
+    //         const result = await apiConnector("GET", categories.CATEGORIES_API);
+    //         console.log("Printing Sublink result:", result);
+    //         setSubLinks(result.data.data);
+    //     }
+    //     catch(error){
+    //         console.log("Could not fetch the category list");
+    //     }
+    // }
+
+    // useEffect( () => {
+    //     fetchSublinks();
+    // })
 
     const location = useLocation();
     const matchRoute = (route) => {
@@ -34,7 +65,31 @@ const Navbar = () => {
                     NavbarLinks.map( (link, index) => (
                         <li key={index}>
                         {
-                            link.title === "Catalog" ? (<div></div>) : (
+                            link.title === "Catalog" ? (
+                                <div className='relative flex items-center gap-2 group'>
+                                    <p>{link.title}</p>
+                                    <IoMdArrowDropdownCircle />
+
+                                    <div className='invisible absolute left-[50%] top-[50%] bg-richblack-5 text-richblack-900
+                                    rounded-md flex flex-col p-4 opacity-0 transition-all duration-200 group-hover:visible
+                                    group-hover:opacity-100 lg:w-[300px] translate-x-[-50%] translate-y-[80%]'>
+
+                                        <div className='absolute left-[50%] h-6 w-6 top-0 bg-richblack-5 rounded rotate-45
+                                        translate-x-[80%] translate-y-[-45%]'>
+                                            
+                                        </div>
+                                        {
+                                                subLinks.length ? (
+                                                    subLinks.map((subLinks, index) => (
+                                                        <Link to={`${subLinks.link}`} key={index}>
+                                                            <p>{subLinks.title}</p>
+                                                        </Link>
+                                                    ))
+                                                ) : (<div></div>)
+                                            }
+                                    </div>
+                                </div>
+                                ) : (
                                 <Link to={link?.path}>
                                     <p className={`${matchRoute(link?.path) ? "text-yellow-25" : "text-richblack-25"}`}>
                                         {link.title}
